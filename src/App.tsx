d0c9969a7ff81157f91fdb14a5153aa946f72eec
yurikaza <yusufmirza55@hotmail.com>
2022-02-14 13:38:09 +0300

    first commti

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from "./Container/Home";
 import { Profile } from "./Container/Profile";
 import { BuyStorageClass } from "./Container/BuyStorage";
-import { MyNavbar } from "./Components/Layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MyNavbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/profile/" component={Profile} />
